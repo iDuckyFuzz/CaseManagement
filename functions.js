4fbee36789f612a9e28f87c6aa1ffe893a1ed352
John Doe <iduckyfuzz@gmail.com>
2020-08-14 14:48:12 +0100

    finalised login

--- functions.js
+++ functions.js
@@ -1,12 +1,20 @@
+let count = 0;
+
 /*This Script allows people to enter by using a form that asks for a
 UserID and Password*/
 function pasuser(form) {
-if (form.id.value=="PLW293") { 
-if (form.pass.value=="Password123") {              
-location="home.html" 
-} else {
-alert("Invalid Password")
-}
-} else {  alert("Invalid UserID")
-}
+    if (form.id.value == "PLW293") {
+        if (form.pass.value == "Password123") {
+            location = "home.html"
+        } else {
+            count++;
+            console.print(count);
+            alert("Invalid Password")
+            if (count === 3) {
+                alert("Your account has been locked please contact 1111!");
+            }
+        }
+    } else {
+        alert("Invalid UserID");
+    }
 }
