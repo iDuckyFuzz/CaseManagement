34d1b4c74656ceef00093aa3af53f2287ef54acf
John Doe <iduckyfuzz@gmail.com>
2020-09-03 16:50:47 +0100

    added modal for succesful submission

--- functions.js
+++ functions.js
@@ -29,8 +29,6 @@ function disputeData(caseType, arn, trxnDate, cnlxDate, amount, mrchName, chrbkR
         this.chrbkRef = chrbkRef
 }
 
-let key = 0;
-
 function submitDispute(form) {
     const caseType = form.elements["casetype"].selectedIndex;
 
@@ -45,16 +43,13 @@ function submitDispute(form) {
     console.log(chargebackData);
     console.log(data);
 
-    localStorage.setItem(key, data);
-    key++;
+    localStorage.setItem(chrbk, data);
+
+    form.reset();
 }
 
 function displaySubmittedDisputes() {
-    const textBox = document.getElementById('data');
-
-    textBox.value = allStorage();
     populateDropdown();
-
 }
 
 function allStorage() {
@@ -76,15 +71,54 @@ function clearStorage() {
 
 function populateDropdown() {
     const values = allStorage();
-    console.log(values);
-
     var submissions = document.getElementById("submissions");
+    keys = Object.keys(localStorage);
+
+    console.log(localStorage);
 
     for (i = 0; i < values.length; i++) {
         var option = document.createElement("OPTION");
-        option.innerHTML = i;
+
+        var retrievedObject = localStorage.getItem(keys[i]);
+        var parsedObject = JSON.parse(retrievedObject);
+        //chargeback reference should be a unique values
+        option.innerHTML = parsedObject.chrbkRef;
         option.value = i;
         submissions.options.add(option);
     }
+    const textBox = document.getElementById('data');
+    keys = Object.keys(localStorage)
+    textBox.value = localStorage.getItem(keys[0]);
+}
 
+function update() {
+    const i = document.getElementById("submissions").value;
+    keys = Object.keys(localStorage)
+    document.getElementById("data").value = localStorage.getItem(keys[i]);
+}
+
+function missingDataCheck() {
+    
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+// Get the button that opens the modal
+var btn = document.getElementById("submit");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
