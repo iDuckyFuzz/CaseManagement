e7624b7c959e373958c949ddea88141a8f9d93fc
John Doe <iduckyfuzz@gmail.com>
2020-09-03 17:04:39 +0100

    updated modal

--- functions.js
+++ functions.js
@@ -45,6 +45,11 @@ function submitDispute(form) {
 
     localStorage.setItem(chrbk, data);
 
+    //add something here to update modal with error message if 
+    //certain detail on the form is not provided
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+
     form.reset();
 }
 
@@ -97,28 +102,19 @@ function update() {
     document.getElementById("data").value = localStorage.getItem(keys[i]);
 }
 
-function missingDataCheck() {
-    
+function submissionCheck() {
+
 }
 
-// Get the modal
 var modal = document.getElementById("myModal");
-// Get the button that opens the modal
-var btn = document.getElementById("submit");
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span.onclick = function () {
+    modal.style.display = "none";
 }
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
